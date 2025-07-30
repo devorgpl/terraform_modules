ac4f1b6d4147e0c7d92bc5c1670f405acc13a42f
Cyprian <cyprian@sniegota.com>
2025-07-31 00:58:21 +0200

    grafana

--- monitoring/monitoring-grafana.tf
+++ monitoring/monitoring-grafana.tf
@@ -9,7 +9,7 @@ resource "helm_release" "grafana" {
   values = [
     templatefile("${path.module}/helm-values/grafana-values.yaml.tftpl", {
       hostname = var.grafana_hostname
-      prometheus_url = "http://prometheus.monitoring.svc.cluster.local:9093"
+      prometheus_url = "http://prometheus-server.monitoring.svc.cluster.local:9093"
       prometheus_alert_url = "http://prometheus-alertmanager.monitoring.svc.cluster.local:9093"
     })
   ]
